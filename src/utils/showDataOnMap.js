d1ee1f577d0a2e04da623306b318424309ec6083
Randeep Singh <rand-singh@users.noreply.github.com>
2020-12-12 22:50:21 +0000

    Popup content and styling

--- src/utils/showDataOnMap.js
+++ src/utils/showDataOnMap.js
@@ -37,7 +37,14 @@ export const showDataOnMap = (data, casesType="cases") => (
             }
         >            
             <Popup>
-                <p>popup</p>
+                <div className="infoContainer">
+                    <div className="infoContainer__flag" style={{backgroundImage: `url(${country.countryInfo.flag})`}}></div>
+                    <div className="infoContainer__name">{country.country}</div>
+                    <div className="infoContainer__confirmed">Cases: {numeral(country.cases).format("0,0")}</div>
+                    <div className="infoContainer__recovered">Recovered: {numeral(country.recovered).format("0,0")}</div>
+                    <div className="infoContainer__deaths">deaths: {numeral(country.deaths).format("0,0")}</div>
+                </div>
+
             </Popup>
         </Circle>
     ))
