45fa2642e230b64a92d7e4a9ac20d49dc382ad8a
Randeep Singh <rand-singh@users.noreply.github.com>
2020-12-18 00:14:13 +0000

    Update Continents.js
    Show continent data

--- src/components/continents/Continents.js
+++ src/components/continents/Continents.js
@@ -19,8 +19,25 @@ function Continents() {
         <section className="continents">
             <h1>Continents Components</h1>
             {
-                continentInfo.map(conti => (
-                    <p>{conti.continent}</p>
+                continentInfo.map((conti) => (
+                    <>
+                        <p>{conti.continent}</p>
+                        <p>Active: {conti.active}</p>
+                        <p>Cases: {conti.cases}</p>
+                        <p>Critical: {conti.critical}</p>
+                        <p>Deaths: {conti.deaths}</p>
+                        <p>Recovered: {conti.recovered}</p>
+                        <p>Tests: {conti.tests}</p>
+                        <br/>
+                        <h1>Todays</h1>
+                        <p>Cases: {conti.todayCases}</p>
+                        <p>Deaths: {conti.todayDeaths}</p>
+                        <p>Recovered: {conti.todayRecovered}</p>
+                        <p>LNG/LAT: {conti.continentInfo.long} - {conti.continentInfo.lat}</p>
+                        <br/>
+                        <hr/>
+                        <br/>                        
+                    </>
                 ))
             }
             
